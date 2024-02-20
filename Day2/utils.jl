6f3b6c71171e872cf42db0a271c016eb5aecd668
Christoph Ortner <christohortner@gmail.com>
2024-02-19 22:36:14 -0800

    eigen example

---
+++ Day2/utils.jl
@@ -0,0 +1,28 @@
+function fdtest(F, dF, x::AbstractVector; verbose=true, p_range=1:10)
+    errors = Float64[]
+    E = F(x)
+    dE = dF(x)
+    # loop through finite-difference step-lengths
+    verbose && @printf("---------|----------- \n")
+    verbose && @printf("    h    | error \n")
+    verbose && @printf("---------|----------- \n")
+    for p = p_range
+       h = 0.1^p
+       dEh = copy(dE)
+       for n = 1:length(dE)
+          x[n] += h
+          dEh[n] = (F(x) - E) / h
+          x[n] -= h
+       end
+       push!(errors, norm(dE - dEh, Inf))
+       verbose && @printf(" %1.1e | %4.2e \n", h, errors[end])
+    end
+    verbose && @printf("---------|----------- \n")
+    if minimum(errors) <= 1e-3 * maximum(errors)
+       verbose && println("passed")
+       return true
+    else
+       @warn("""It seems the finite-difference test has failed""")
+       return false
+    end
+ end
